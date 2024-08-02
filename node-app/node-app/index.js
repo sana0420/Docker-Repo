5b9d105619a0d147a9de31d68e1a83fd5c00576f
sana arshad <115351368+sana0420@users.noreply.github.com>
2024-08-02 23:52:23 +0500

    index.js

--- node-app/node-app/index.js
+++ node-app/node-app/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello I'm  sana! This is my node project.\n'
+  const msg = 'Hello I am  sana! This is my node project.\n'
   res.end(msg);
 });
 
